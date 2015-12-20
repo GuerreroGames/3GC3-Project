b37974dc0795f7e6989c2b52de6a1cbb93279bc8
GuerreroGames <alexpguerrero@gmail.com>
2015-12-19 20:26:08 -0500

    updated ghost class

--- Ghost.cpp
+++ Ghost.cpp
@@ -4,32 +4,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+
 #include "Ghost.h"
+#include "Vector3D"
 
 using namespace std;
 
 Ghost::Ghost()
 {
-	bool status = true;
 	float pos[3] = {0.0, 9.0, 0.0};
 	float rot[3] = {0, 0, 0};
 	string dir = "forward";
+	bool vertNormsCalced = false;
+	Vector3D vertNorms[23];
 }
 
 
 Ghost::Ghost(float x, float z)
 {
-	bool status = true;
 	float pos[3] = {x, 9.0, z};
 	float rot[3] = {0, 0, 0};
 	string dir = "forward";
+	bool vertNormsCalced = false;
+	Vector3D vertNorms[23];
 }
 
 
 Ghost::Ghost(float x, float z, string startDir)
 {
-	bool status = true;
 	float pos[3] = {x, 9.0, z};
+	bool vertNormsCalced = false;
+	Vector3D vertNorms[23];
 
 	if(startDir == "forward")
 	{
@@ -85,8 +90,29 @@ void Ghost::move(std::string moveDir)
 }
 
 
-void Ghost::draw()
+Vector3D normAvg(Vector3D fNorm1, Vector3D fNorm2,
+				 Vector3D fNorm3, Vector3D fNorm4 )
+{	/* 
+	 * Takes the face normals and calcs the
+	 * corresponding vertex normal
+	 */
+	Vector3D vertNorm = Vector3D(fNorm1.x + fNorm2.x + fNorm3.x + fNorm4.x,
+								 fNorm1.y + fNorm2.y + fNorm3.y + fNorm4.y,
+								 fNorm1.z + fNorm2.z + fNorm3.z + fNorm4.z )
+	return vertNorm;
+}
+
+
+void calcVertNorms()
 {
+	
+}
+
+
+void Ghost::draw()
+{   
+	if(vertNormsCalced != true){ calcVertNorms(); }
+
 	glPushMatrix(); // ghost body
 
 	glTranslatef(pos[0], pos[1], pos[2]);
@@ -128,92 +154,92 @@ void Ghost::draw()
 	glPushMatrix(); // ghost tail
 	glTranslatef(0.0, -5, 0.0);
 	glColor3f(1,1,1);
-	glBegin(GL_POLYGON);
+	glBegin(GL_QUADS);
 
-		glVertex3f(3.536, 5.0, -3.536);
-		glVertex3f(2.337, 3.840, -4.006);
-		glVertex3f(0.0, 3.840, -5.471);
-		glVertex3f(0.0, 5.0, -5.0);
+		glVertex3f(3.536, 5.0, -3.536); // p1
+		glVertex3f(2.337, 3.840, -4.006); // p4 
+		glVertex3f(0.0, 3.840, -5.471); // p5
+		glVertex3f(0.0, 5.0, -5.0); // p2
 
-		glVertex3f(0.0, 5.0, -5.0);
-		glVertex3f(0.0, 3.840, -5.471);
-		glVertex3f(-2.337, 3.840, -4.006);
-		glVertex3f(-3.536, 5.0, -3.536);
+		glVertex3f(0.0, 5.0, -5.0); // p2
+		glVertex3f(0.0, 3.840, -5.471); // p5
+		glVertex3f(-2.337, 3.840, -4.006); // p6
+		glVertex3f(-3.536, 5.0, -3.536); // p3
 
-		glVertex3f(3.536, 5.0, -3.536);
-		glVertex3f(3.266, 3.087, -3.266);
-		glVertex3f(1.009, 2.963, -6.363);
-		glVertex3f(2.337, 3.840, -4.006);
+		glVertex3f(3.536, 5.0, -3.536); // p1
+		glVertex3f(3.266, 3.087, -3.266); // p7
+		glVertex3f(1.009, 2.963, -6.363); // p8
+		glVertex3f(2.337, 3.840, -4.006); // p6
 
-		glVertex3f(-2.337, 3.840, -4.006);
-		glVertex3f(-1.009, 2.963, -6.363);
-		glVertex3f(-3.266, 3.087, -3.266);
-		glVertex3f(-3.536, 5.0, -3.536);
+		glVertex3f(-2.337, 3.840, -4.006); // p6
+		glVertex3f(-1.009, 2.963, -6.363); // p10
+		glVertex3f(-3.266, 3.087, -3.266); // p11
+		glVertex3f(-3.536, 5.0, -3.536); // p3
 
-		glVertex3f(2.337, 3.840, -4.006);
-		glVertex3f(1.009, 2.963, -6.363);
-		glVertex3f(0.0, 2.963, -7.716);
-		glVertex3f(0.0, 3.840, -5.471);
+		glVertex3f(2.337, 3.840, -4.006); // p4
+		glVertex3f(1.009, 2.963, -6.363); // p8
+		glVertex3f(0.0, 2.963, -7.716); // p9
+		glVertex3f(0.0, 3.840, -5.471); // p5
 
-		glVertex3f(0.0, 3.840, -5.471);
-		glVertex3f(0.0, 2.963, -7.716);
-		glVertex3f(-1.009, 2.963, -6.363);
-		glVertex3f(-2.337, 3.840, -4.006);
+		glVertex3f(0.0, 3.840, -5.471); // p5
+		glVertex3f(0.0, 2.963, -7.716); // p9
+		glVertex3f(-1.009, 2.963, -6.363); // p10
+		glVertex3f(-2.337, 3.840, -4.006); // p6
 
-		glVertex3f(3.266, 3.087, -3.266);
-		glVertex3f(2.5, 1.464, -2.5);
-		glVertex3f(0.824, 1.454, -5.1);
-		glVertex3f(1.009, 2.963, -6.363);
+		glVertex3f(3.266, 3.087, -3.266); // p7
+		glVertex3f(2.5, 1.464, -2.5); // p12
+		glVertex3f(0.824, 1.454, -5.1); // p13
+		glVertex3f(1.009, 2.963, -6.363); // p8
 
-		glVertex3f(-1.009, 2.963, -6.363);
-		glVertex3f(-0.824, 1.454, -5.1);
-		glVertex3f(-2.5, 1.464, -2.5);
-		glVertex3f(-3.266, 3.087, -3.266);
+		glVertex3f(-1.009, 2.963, -6.363); // p10
+		glVertex3f(-0.824, 1.454, -5.1); // p15
+		glVertex3f(-2.5, 1.464, -2.5); // p9
+		glVertex3f(-3.266, 3.087, -3.266); // p11
 
-		glVertex3f(1.009, 2.963, -6.363);
-		glVertex3f(0.824, 1.454, -5.1);
-		glVertex3f(0.0, 1.454, -6.135);
-		glVertex3f(0.0, 2.963, -7.716);
+		glVertex3f(1.009, 2.963, -6.363); // p8
+		glVertex3f(0.824, 1.454, -5.1); // p13
+		glVertex3f(0.0, 1.454, -6.135); // p14
+		glVertex3f(0.0, 2.963, -7.716); // p9
 
-		glVertex3f(0.0, 2.963, -7.716);
-		glVertex3f(0.0, 1.454, -6.135);
-		glVertex3f(-0.824, 1.454, -5.1);
-		glVertex3f(-1.009, 2.963, -6.363);
+		glVertex3f(0.0, 2.963, -7.716); // p9
+		glVertex3f(0.0, 1.454, -6.135); // p14
+		glVertex3f(-0.824, 1.454, -5.1); // p15
+		glVertex3f(-1.009, 2.963, -6.363); // p10
 
-		glVertex3f(2.5, 1.464, -2.5);
-		glVertex3f(1.353, 0.381, -1.353);
-		glVertex3f(0.605, 0.364, -3.261);
-		glVertex3f(0.824, 1.454, -5.1);
+		glVertex3f(2.5, 1.464, -2.5); // p12
+		glVertex3f(1.353, 0.381, -1.353); // p17
+		glVertex3f(0.605, 0.364, -3.261); // p18
+		glVertex3f(0.824, 1.454, -5.1); // p13
 
-		glVertex3f(-0.824, 1.454, -5.1);
-		glVertex3f(-0.605, 0.364, -3.261);
-		glVertex3f(-1.353, 0.381, -1.353);
-		glVertex3f(-2.5, 1.464, -2.5);
+		glVertex3f(-0.824, 1.454, -5.1); // p15
+		glVertex3f(-0.605, 0.364, -3.261); // p20
+		glVertex3f(-1.353, 0.381, -1.353); // p21
+		glVertex3f(-2.5, 1.464, -2.5); // p16
 
-		glVertex3f(0.824, 1.454, -5.1);
-		glVertex3f(0.605, 0.364, -3.261);
-		glVertex3f(0.0, 0.364, -3.821);
-		glVertex3f(0.0, 1.454, -6.135);
+		glVertex3f(0.824, 1.454, -5.1); // p13
+		glVertex3f(0.605, 0.364, -3.261); // p18
+		glVertex3f(0.0, 0.364, -3.821); // p19
+		glVertex3f(0.0, 1.454, -6.135); // p14
 
-		glVertex3f(0.0, 1.454, -6.135);
-		glVertex3f(0.0, 0.364, -3.821);
-		glVertex3f(-0.605, 0.364, -3.261);
-		glVertex3f(-0.824, 1.454, -5.1);
+		glVertex3f(0.0, 1.454, -6.135); // p14
+		glVertex3f(0.0, 0.364, -3.821); // p19
+		glVertex3f(-0.605, 0.364, -3.261); // p20
+		glVertex3f(-0.824, 1.454, -5.1); // p15
 
-		glVertex3f(1.353, 0.381, -1.353);
-		glVertex3f(0.0, 0.0, -1.832);
-		glVertex3f(0.0, 0.364, -3.821);
-		glVertex3f(0.605, 0.364, -3.261);
+		glVertex3f(1.353, 0.381, -1.353); // p17
+		glVertex3f(0.0, 0.0, -1.832); // p22
+		glVertex3f(0.0, 0.364, -3.821); // p19
+		glVertex3f(0.605, 0.364, -3.261); // p18
 
-		glVertex3f(-0.605, 0.364, -3.261);
-		glVertex3f(0.0, 0.364, -3.821);
-		glVertex3f(0.0, 0.0, -1.832);
-		glVertex3f(-1.353, 0.381, -1.353);
+		glVertex3f(-0.605, 0.364, -3.261); // p20
+		glVertex3f(0.0, 0.364, -3.821); // p19
+		glVertex3f(0.0, 0.0, -1.832); // p22
+		glVertex3f(-1.353, 0.381, -1.353); // p21
 
-		glVertex3f(1.353, 0.381, -1.353);
-		glVertex3f(0.0, 0.0, 0.0);
-		glVertex3f(-1.353, 0.381, -1.353);
-		glVertex3f(0.0, 0.0, -1.832);
+		glVertex3f(1.353, 0.381, -1.353); // p17
+		glVertex3f(0.0, 0.0, 0.0); // p23
+		glVertex3f(-1.353, 0.381, -1.353); // p21
+		glVertex3f(0.0, 0.0, -1.832); // p22
 
 	glEnd();
 	
--- Ghost.h
+++ Ghost.h
@@ -1,19 +1,23 @@
 #ifndef __GHOST_H__
 #define __GHOST_H__
 #include <string>
+#include "Vector3D"
 
 class Ghost{
 public:
-	bool status;
 	float pos[3];
 	float rot[3];
 	std::string dir;
+	bool vertNormsCalced;
+	Vector3D vertNorms[23];
 
 	Ghost();
 	Ghost(float x, float z);
 	Ghost(float x, float z, std::string startDir);
 
 	void move(std::string moveDir);
+	Vector3D normAvg(Vector3D fNorm1, Vector3D fNorm2,
+					 Vector3D fNorm3, Vector3D fNorm4 )
 	void draw();
 };
 #endif
