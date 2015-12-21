ddc5722ff207d006b1f7483682a366e231a6c270
Alexander Guerrero <guerreap@mcmaster.ca>
2015-12-20 19:25:18 -0500

    Added Syed's files

---
+++ README.txt
@@ -0,0 +1,21 @@
+
+~* README *~
+
+
+SE/CS 3GC3 - FINAL PROJECT
+Date: December 20, 2015
+
+Group Members: 	Karl Knopf (1437217)
+         	 	Syed Gardezi (1074314)
+          		Alexander Guerrero (1133763)
+
+
+Welcome to Night Light! You are a ghost trying to escape an haunted
+mansion in order release your spirit and rest in peace. Ghosts do not
+like light and will "spooked" if they are hit by too much of it.
+Navitage through the corridors, avoid the light, free your spirit
+and win the game!
+
+CONTROLS:
+
+	W, A, S, D - move ghost
---
+++ start.cpp
@@ -0,0 +1,60 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void out_string_at(void *font, int x, int y, char *string)
+{
+#ifdef __FREEGLUT_EXT_H__
+    glRasterPos2f((GLfloat)x, (GLfloat)y);
+    glutBitmapString( font, (const unsigned char *)string );
+#else
+    int len, i;
+    glRasterPos2f((GLfloat)x, (GLfloat)y);
+    len = (int) strlen(string);
+    for (i = 0; i < len; i++) {
+        glutBitmapCharacter(font, string[i]);
+    }
+#endif
+}
+
+static void init(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glShadeModel (GL_FLAT);
+}
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0,0.0,0.0);
+    out_string_at( GLUT_BITMAP_TIMES_ROMAN_24, 250, 300, "Start Game" );
+    glFlush ();
+}
+static void reshape(int w, int h){
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    gluOrtho2D (0.0, (GLdouble) w, 0.0, (GLdouble) h);
+}
+static void keyboard(unsigned char key, int x, int y)
+{
+    if((key == 0x1b)||(key == 'q')||(key == 'Q'))
+        exit(0);
+}
+int main (int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (600, 600);
+    glutCreateWindow ("");
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
+}
